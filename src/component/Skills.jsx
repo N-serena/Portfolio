274384c182354500bce6df394c24d8fbe351de55
N-serena <nobserena@gmail.com>
2025-07-08 17:04:07 -0400

    details

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -1,6 +1,6 @@
 import { DiFirebase } from "react-icons/di";
 import { FaPython, FaJava, FaReact, FaDatabase, FaAws } from "react-icons/fa";
-import { SiSpringboot, SiTailwindcss, SiGit, SiJavascript } from "react-icons/si";
+import { SiSpringboot, SiTailwindcss, SiGit, SiJavascript, SiPostman, SiJunit5 } from "react-icons/si";
 
 const Skills = ({ theme }) => (
   // Skills Section
@@ -20,32 +20,40 @@ const Skills = ({ theme }) => (
         <span className="mt-2 text-sm">Spring Boot</span>
       </div>
       <div className="flex flex-col items-center">
+        <SiJavascript className="text-4xl text-[#f7df1e]" />
+        <span className="mt-2 text-sm">JavaScript</span>
+      </div>
+      <div className="flex flex-col items-center">
         <FaReact className="text-4xl text-[#61dbfb]" />
         <span className="mt-2 text-sm">React</span>
-      </div>
-      <div className="flex flex-col items-center">
-        <FaDatabase className="text-4xl text-[#336791]" />
-        <span className="mt-2 text-sm">SQL</span>
-      </div>
-      <div className="flex flex-col items-center">
-        <FaAws className="text-4xl text-[#FF9900]" />
-        <span className="mt-2 text-sm">AWS</span>
       </div>
       <div className="flex flex-col items-center">
         <SiTailwindcss className="text-4xl text-[#38bdf8]" />
         <span className="mt-2 text-sm">Tailwind</span>
       </div>
       <div className="flex flex-col items-center">
-        <SiGit className="text-4xl text-[#f34f29]" />
-        <span className="mt-2 text-sm">Git</span>
-      </div>
-      <div className="flex flex-col items-center">
-        <SiJavascript className="text-4xl text-[#f7df1e]" />
-        <span className="mt-2 text-sm">JavaScript</span>
+        <FaDatabase className="text-4xl text-[#336791]" />
+        <span className="mt-2 text-sm">SQL</span>
       </div>
       <div className="flex flex-col items-center">
         <DiFirebase className="text-4xl text-[#ffca28]" />
         <span className="mt-2 text-sm">Firebase</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <FaAws className="text-4xl text-[#FF9900]" />
+        <span className="mt-2 text-sm">AWS</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <SiGit className="text-4xl text-[#f34f29]" />
+        <span className="mt-2 text-sm">Git</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <SiPostman className="text-4xl text-[#ff6c37]" />
+        <span className="mt-2 text-sm">Postman</span>
+      </div>
+      <div className="flex flex-col items-center">
+        <SiJunit5 className="text-4xl text-[#25A162]" />
+        <span className="mt-2 text-sm">JUnit 5</span>
       </div>
       {/* Add/remove icons and skills as you like! */}
     </div>
